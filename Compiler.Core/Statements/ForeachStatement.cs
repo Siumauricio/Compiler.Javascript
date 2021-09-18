c77a74005c9eede247c582fbd3edddb0a8138156
Enrique Cruz <enrique.cs095@unitec.edu>
2021-09-17 18:20:47 -0600

    Added Foreach statement

---
+++ Compiler.Core/Statements/ForeachStatement.cs
@@ -0,0 +1,41 @@
+﻿using Compiler.Core.Expressions;
+using Compiler.Core.Interfaces;
+using System;
+
+namespace Compiler.Core.Statements
+{
+    public class ForEachStatement : Statement
+    {
+        public ForEachStatement(TypedExpression expression, Statement statement)
+        {
+            Expression = expression;
+            Statement = statement;
+        }
+
+        public ForEachStatement(Statement statement)
+        {
+            Statement = statement;
+        }
+
+        public TypedExpression Expression { get; }
+        public Statement Statement { get; }
+
+        public override void ValidateSemantic()
+        {
+            if (Expression.GetExpressionType() != Type.Bool)
+            {
+                throw new ApplicationException("Parameters are required in for bucle");
+            }
+        }
+
+        public override void Interpret()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Generate(int tabs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
